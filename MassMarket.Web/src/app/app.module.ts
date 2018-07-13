e50d9483406ce1c3e78ed9ed04360d467ca773a2
Dennis van Hest <dennisvhest@hotmail.nl>
2018-07-13 16:24:44 +0200

    Project setup

---
+++ MassMarket.Web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
